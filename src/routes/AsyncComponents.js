6453cc298cc03bf8351fdf02dafaf020b4f905f7
Michael <michaeldoit.dev@gmail.com>
2019-08-08 11:33:52 +0700

    re name folder container to page for code spliting

--- src/routes/AsyncComponents.js
+++ src/routes/AsyncComponents.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Loadable from 'react-loadable';
 
 export const AsyncPageDefault = Loadable({
-  loader: () => import(/* webpackChunkName: "PageDefault" */ '../containers/PageDefault'),
+  loader: () => import(/* webpackChunkName: "PageDefault" */ '../pages/PageDefault'),
   loading: () => <div>loading...</div>,
   modules: ['PageDefault'],
 });
 
 
 export const AsyncPageOther = Loadable({
-  loader: () => import(/* webpackChunkName: "PageOther" */ '../containers/PageOther'),
+  loader: () => import(/* webpackChunkName: "PageOther" */ '../pages/PageOther'),
   loading: () => <div>loading...</div>,
   modules: ['PageOther'],
 });
